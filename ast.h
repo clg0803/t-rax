50b4d76b3eb1d36752e6701ef829015c301a7dde
clg0803 <coder_guanchenlu@163.com>
2022-09-18 15:14:29 +0800

    add AST def

---
+++ ast.h
@@ -0,0 +1,69 @@
+#include <string>
+#include <memory>
+#include <vector>
+
+namespace ast
+{
+    class ExprAST
+    {
+        public:
+            virtual ~ExprAST() = default;
+    };
+
+    class NumberExprAst : public ExprAST
+    {
+        double Val;
+    public:
+        NumberExprAst(double Val) : Val(Val){}
+    };
+
+    class VariableExprAst : public ExprAST
+    {
+        std::string Name;
+    public:
+        VariableExprAst(const std::string& name) : Name(name) {}
+    };
+
+    class BinaryExprAST : public ExprAST
+    {
+        char Op;
+        std::unique_ptr<ExprAST> LHS, RHS;
+
+    public:
+        BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
+        : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+    };
+
+    // function call 
+    class CallExprAST : public ExprAST {
+        std::string Callee;
+        std::vector<std::unique_ptr<ExprAST>> Args;
+
+    public:
+        CallExprAST(const std::string &Callee, 
+            std::vector<std::unique_ptr<ExprAST>> Args)
+        : Callee(Callee), Args(std::move(Args)) {}
+    };
+
+    // function prototype ~~ `func_name(arg1, arg2, ...)`
+    class PrototypeAST {
+        std::string Name;
+        std::vector<std::string> Args;
+
+    public:
+        PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+            : Name(Name), Args(std::move(Args)) {}
+
+        const std::string &getName() const { return Name; }
+    };
+
+    class FunctionAST {
+        std::unique_ptr<PrototypeAST> Proto;
+        std::unique_ptr<ExprAST> Body;
+
+    public:
+        FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+                    std::unique_ptr<ExprAST> Body)
+            : Proto(std::move(Proto)), Body(std::move(Body)) {}
+    };
+}
